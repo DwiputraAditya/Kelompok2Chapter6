899043b6a98d3738748e6e10c6852d28be14f948
DwiputraAditya <you@example.com>
2023-05-14 13:54:23 +0700

    Base Project

---
+++ Challenge6/src/main/java/com/binar/challenge4/repository/SeatRepository.java
@@ -0,0 +1,13 @@
+package com.binar.challenge4.repository;
+
+import com.binar.challenge4.model.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    Seat findSeatsBySeatNumberAndStudio(String seatNumber, String studio);
+}
---
+++ Challenge6/src/main/java/com/binar/challenge4/service/SeatService.java
@@ -0,0 +1,31 @@
+package com.binar.challenge4.service;
+
+import com.binar.challenge4.model.Seat;
+import com.binar.challenge4.repository.SeatRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SeatService {
+    @Autowired
+    private SeatRepository seatRepository;
+
+    public List<Seat> getAllSeat(){
+        return seatRepository.findAll();
+    }
+
+    public Seat addSeats(String seatNumber, boolean isAvailable, String studioName) throws Exception {
+        Seat seats = seatRepository.findSeatsBySeatNumberAndStudio(seatNumber, studioName);
+        if(seats != null){
+            throw new Exception("Seats already exist");
+        }
+        Seat seat = new Seat();
+        seat.setSeatNumber(seatNumber);
+        seat.setIsAvailable(isAvailable);
+        seat.setStudio(studioName);
+
+        return seatRepository.save(seat);
+    }
+}
