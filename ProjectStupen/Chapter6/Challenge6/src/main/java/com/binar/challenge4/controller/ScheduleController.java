af89c83d073a8712295088ea6aad3cccf2df21f9
ayunurqanita <125667589+ayunurqanita@users.noreply.github.com>
2023-05-18 00:22:12 +0800

    Update

--- ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/ScheduleController.java
+++ ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/ScheduleController.java
@@ -5,41 +5,48 @@ import com.binar.challenge4.model.Schedule;
 import com.binar.challenge4.service.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/schedule")
+@RequestMapping("/api/schedule")
 public class ScheduleController {
     @Autowired
     private ScheduleService scheduleService;
 
     @PostMapping("/addSchedule")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Schedule> addSchedule(@RequestBody ScheduleDTO schedule, @RequestParam Long filmCode){
         Schedule schedule1 = scheduleService.addSchedule(schedule, filmCode);
         return ResponseEntity.ok(schedule1);
     }
 
     @GetMapping("/getScheduleByFilmCode")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<Schedule>> getScheduleByFilmCode(@RequestParam(name = "filmCode") Long filmCode){
         List<Schedule> allSchedule = scheduleService.getScheduleByFilmCode(filmCode);
         return ResponseEntity.ok(allSchedule);
     }
 
     @GetMapping("/getScheduleByFilmName")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<Schedule>> getScheduleByFilmCode(@RequestParam(name = "filmName") String filmName){
         List<Schedule> Schedule = scheduleService.getScheduleByFilmName(filmName);
         return ResponseEntity.ok(Schedule);
     }
 
     @GetMapping("/getScheduleByFilmIsPremiered")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<Schedule>> getScheduleByFilmIsPremiered(@RequestParam(name = "isPremiered") Boolean isPremiered){
         List<Schedule> schedule = scheduleService.getScheduleByFilmIsPremiered(isPremiered);
         return ResponseEntity.ok(schedule);
     }
 
     @GetMapping("/getAllSchedule")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public List<Schedule> getAllSchedule(){
         return scheduleService.getAllSchedule();
     }
