87c15b4b6d4054089a01cced1639d88f08bbe7f7
ayunurqanita <125667589+ayunurqanita@users.noreply.github.com>
2023-05-18 00:20:09 +0800

    Update

--- ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/FilmController.java
+++ ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/FilmController.java
@@ -6,23 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/film")
+@RequestMapping("/api/film")
 public class FilmController {
     @Autowired
     private FilmService filmService;
-
     @PostMapping("/addFilm")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Film> addFilm(@RequestBody Film film) {
         Film film1 = filmService.addFilm(film);
         return new ResponseEntity<>(film1, HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @GetMapping("/getAllFilm")
     public ResponseEntity<List<Film>> getAllFilm() {
         List<Film> allFilm = filmService.getAllFilm();
@@ -30,11 +33,13 @@ public class FilmController {
     }
 
     @GetMapping("/getAllFilmPagination")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public Page<Film> getAllUsersPagination(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
         return filmService.getAllFilmPagination(page, size);
     }
 
     @GetMapping("/getFilmById/{filmId}")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<Film> getFilmById(@RequestParam("filmCode") Long id) {
         Optional<Film> film = filmService.getFilmById(id);
         if (film.isPresent()) {
@@ -45,18 +50,21 @@ public class FilmController {
     }
 
     @GetMapping("/getFilmByIsPremiered")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<Film>> getFilmByIsPremiered(@RequestParam(name = "isPremiered") Boolean isPremiered){
         List<Film> schedule = filmService.getScheduleByFilmIsPremiered(isPremiered);
         return ResponseEntity.ok(schedule);
     }
 
     @PutMapping("/updateFilm")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> updateFilm(@RequestParam(name = "filmCode") Long id, @RequestBody Film film) {
         filmService.updateFilm(id, film);
         return new ResponseEntity<String>("Data berhasil di update", HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteFilmById/{filmId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deleteFilmById(@RequestParam(name = "filmCode") Long id) {
         filmService.deleteFilmById(id);
         return ResponseEntity.ok("Data Berhasil dihapus");
