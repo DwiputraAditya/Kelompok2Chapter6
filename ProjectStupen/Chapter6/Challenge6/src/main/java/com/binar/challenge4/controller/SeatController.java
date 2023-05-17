e5dfb6c9ba9ded3a228d6e378e0ff648bbca5d66
ayunurqanita <125667589+ayunurqanita@users.noreply.github.com>
2023-05-18 00:22:42 +0800

    Update

--- ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/SeatController.java
+++ ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/SeatController.java
@@ -5,17 +5,20 @@ import com.binar.challenge4.service.SeatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/seats")
+@RequestMapping("/api/seats")
 public class SeatController {
     @Autowired
     SeatService seatService;
 
     @GetMapping("/getAllSeat")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<Seat>> getAllSeat(){
         List<Seat> seats = seatService.getAllSeat();
         return ResponseEntity.ok(seats);
@@ -23,6 +26,7 @@ public class SeatController {
     }
 
     @PostMapping("/addSeat")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Seat> addSeat(@RequestParam(name = "seat_number") String seatNumber, @RequestParam(name = "is_available") boolean isAvailable, @RequestParam(name = "studio") String studioName) throws Exception{
         Seat addSeat = seatService.addSeats(seatNumber, isAvailable, studioName);
         return new ResponseEntity<>(addSeat, HttpStatus.CREATED);
