7c4d1c7d18dd2313aa140131e71468ca34e3f18b
ayunurqanita <125667589+ayunurqanita@users.noreply.github.com>
2023-05-18 00:21:20 +0800

    Update InvoiceController.java

--- ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/InvoiceController.java
+++ ProjectStupen/Chapter6/Challenge6/src/main/java/com/binar/challenge4/controller/InvoiceController.java
@@ -7,13 +7,15 @@ import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/booking")
+@RequestMapping("/api/booking")
 public class InvoiceController {
     @Autowired
     private InvoiceService invoiceService;
@@ -23,12 +25,14 @@ public class InvoiceController {
 
 
     @GetMapping("/getAllInvoice")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public ResponseEntity<List<Invoice>> getAllInvoice() {
         List<Invoice> allInvoice = invoiceService.getAllInvoice();
         return ResponseEntity.ok(allInvoice);
     }
 
     @GetMapping("/generateInvoice")
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     public void getInvoiceReport(Long invoiceId) throws Exception{
         response.setContentType("application/pdf");
         response.setHeader("Content-disposition", "attachment; filename=\"invoice.pdf\"");
@@ -37,6 +41,7 @@ public class InvoiceController {
     }
 
     @PostMapping("/addBooking")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Invoice> addDataForBooking(@RequestParam(name = "film_code") Long filmCode, @RequestParam(name = "schedule_id") Long scheduleId, @RequestParam(name = "seat_id") Long seatId) throws Exception {
         try {
             Invoice addDataForBooking = invoiceService.addDataForBooking(filmCode, scheduleId, seatId);
